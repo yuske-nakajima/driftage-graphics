5468c5d1d30c9f0f3b86d990bbd42ade24a0aa6d
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-06-26 00:44:51 +0900

    まっすぐじゃない線を等間隔で引く-動くように修正

--- src/pages/art/line/simple/noise.tsx
+++ src/pages/art/line/simple/noise.tsx
@@ -2,6 +2,7 @@ import DefaultSketch from '@/components/pages/DefaultSketch'
 import { drawBlock, fitCreateCanvas, noiseLine } from '@/lib/functions'
 import { PageInfo } from '@/lib/types'
 import type { Sketch } from '@p5-wrapper/react'
+import { Vector } from 'p5'
 
 export const pageInfo: PageInfo = {
   title: '上からまっすぐじゃない線を引く',
@@ -10,27 +11,34 @@ export const pageInfo: PageInfo = {
 }
 
 const sketch: Sketch = (p5) => {
+  const noiseLineList: { start: Vector; end: Vector }[] = []
+
   p5.setup = () => {
     fitCreateCanvas(p5)
     p5.colorMode(p5.HSB)
-
-    p5.background(0, 0, 95)
+    p5.frameRate(24)
 
     const div = p5.height / 10
 
     for (let i = 1; i * div < p5.height; i++) {
-      drawBlock(p5, () => {
-        p5.stroke(0, 0, 0)
-        p5.strokeWeight(i)
-        p5.noFill()
-        noiseLine(
-          p5,
-          p5.createVector(0, i * div),
-          p5.createVector(p5.width, i * div),
-          10,
-        )
+      noiseLineList.push({
+        start: p5.createVector(0, i * div),
+        end: p5.createVector(p5.width, i * div),
       })
     }
+  }
+
+  p5.draw = () => {
+    p5.background(0, 0, 95)
+
+    noiseLineList.forEach((item, i) => {
+      drawBlock(p5, () => {
+        p5.stroke(0, 0, 0)
+        p5.strokeWeight(i + 1)
+        p5.noFill()
+        noiseLine(p5, item.start, item.end, 10)
+      })
+    })
   }
 }
 
