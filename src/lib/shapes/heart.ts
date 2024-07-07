c81c49e404554109e171b55dcd46150a794db0fa
yusuke nakajima <yuske@developer-nakajima.xyz>
2024-07-08 01:12:53 +0900

    ハートをオブジェクトに変換

---
+++ src/lib/shapes/heart.ts
@@ -0,0 +1,100 @@
+import { drawBlock } from '@/lib/functions'
+import type { P5CanvasInstance } from '@p5-wrapper/react'
+import { Vector } from 'p5'
+
+export class Heart {
+  p5: P5CanvasInstance
+
+  topPoint: Vector
+  underPoint: Vector
+  leftControl1: Vector
+  leftControl2: Vector
+  rightControl1: Vector
+  rightControl2: Vector
+
+  constructor(p5: P5CanvasInstance) {
+    this.p5 = p5
+    this.topPoint = p5.createVector()
+    this.underPoint = p5.createVector()
+    this.leftControl1 = p5.createVector()
+    this.leftControl2 = p5.createVector()
+    this.rightControl1 = p5.createVector()
+    this.rightControl2 = p5.createVector()
+  }
+
+  public set(center: Vector) {
+    const p5 = this.p5
+
+    this.topPoint = p5.createVector(center.x, center.y - 100)
+    this.underPoint = p5.createVector(center.x, center.y + 100)
+    this.leftControl1 = p5.createVector(
+      p5.max(0, center.x - 50),
+      center.y - 200,
+    )
+    this.leftControl2 = p5.createVector(
+      p5.max(0, center.x - 400),
+      center.y - 100,
+    )
+    this.rightControl1 = p5.createVector(
+      p5.min(p5.width, center.x + 50),
+      center.y - 200,
+    )
+    this.rightControl2 = p5.createVector(
+      p5.min(p5.width, center.x + 400),
+      center.y - 100,
+    )
+  }
+
+  public get() {
+    return {
+      topPoint: this.topPoint,
+      underPoint: this.underPoint,
+      leftControl1: this.leftControl1,
+      leftControl2: this.leftControl2,
+      rightControl1: this.rightControl1,
+      rightControl2: this.rightControl2,
+    }
+  }
+
+  public add(value: Vector) {
+    this.topPoint = this.topPoint.add(value)
+    this.underPoint = this.underPoint.add(value)
+    this.leftControl1 = this.leftControl1.add(value)
+    this.leftControl2 = this.leftControl2.add(value)
+    this.rightControl1 = this.rightControl1.add(value)
+    this.rightControl2 = this.rightControl2.add(value)
+  }
+
+  public draw() {
+    const p5 = this.p5
+
+    drawBlock(p5, () => {
+      p5.fill(p5.frameCount % 360, 100, 100)
+      p5.strokeWeight(10)
+
+      p5.beginShape()
+      p5.vertex(this.topPoint.x, this.topPoint.y)
+
+      // ベジェ曲線でハートのカーブを表現
+      p5.bezierVertex(
+        this.leftControl1.x,
+        this.leftControl1.y,
+        this.leftControl2.x,
+        this.leftControl2.y,
+        this.underPoint.x,
+        this.underPoint.y,
+      )
+      // 右側
+      p5.bezierVertex(
+        this.rightControl2.x,
+        this.rightControl2.y,
+        this.rightControl1.x,
+        this.rightControl1.y,
+        this.topPoint.x,
+        this.topPoint.y,
+      )
+
+      p5.endShape(p5.CLOSE)
+    })
+  }
+}
--- src/pages/art/shape/heart/noise.tsx
+++ src/pages/art/shape/heart/noise.tsx
@@ -1,5 +1,6 @@
 import DefaultSketch from '@/components/pages/DefaultSketch'
-import { drawBlock, fitCreateCanvas, noisyPoint } from '@/lib/functions'
+import { fitCreateCanvas } from '@/lib/functions'
+import { Heart } from '@/lib/shapes/heart'
 import { PageInfo } from '@/lib/types'
 import type { Sketch } from '@p5-wrapper/react'
 import { Vector } from 'p5'
@@ -10,78 +11,36 @@ export const pageInfo: PageInfo = {
   href: 'art/shape/heart/noise',
 }
 
-const moveLevel = 10
-
 const sketch: Sketch = (p5) => {
   let center: Vector
-  let wq: number
-  let hq: number
+  const step = p5.createVector(5, 10)
+
+  const heart = new Heart(p5)
 
   p5.setup = () => {
     fitCreateCanvas(p5)
     p5.colorMode(p5.HSB)
-    p5.frameRate(12)
+    p5.frameRate(24)
 
     center = p5.createVector(p5.width / 2, p5.height / 2)
-    wq = p5.width / 4
-    hq = p5.height / 4
+    heart.set(center)
   }
 
   p5.draw = () => {
-    const topPoint = p5.createVector(
-      noisyPoint(p5, center.x, moveLevel),
-      noisyPoint(p5, hq + hq / 3, moveLevel),
-    )
-    const underPoint = p5.createVector(
-      noisyPoint(p5, center.x, moveLevel),
-      noisyPoint(p5, hq * 3 + hq / 2, moveLevel),
-    )
-    const leftControl1 = p5.createVector(
-      noisyPoint(p5, wq * 1.5, moveLevel),
-      noisyPoint(p5, hq - hq / 2, moveLevel),
-    )
-    const leftControl2 = p5.createVector(
-      noisyPoint(p5, wq - wq * 1.2, moveLevel),
-      noisyPoint(p5, center.y - hq * 1.2, moveLevel),
-    )
-    const rightControl1 = p5.createVector(
-      noisyPoint(p5, p5.width - wq + wq * 1.2, moveLevel),
-      noisyPoint(p5, center.y - hq * 1.2, moveLevel),
-    )
-    const rightControl2 = p5.createVector(
-      noisyPoint(p5, p5.width - wq * 1.5, moveLevel),
-      noisyPoint(p5, hq - hq / 2, moveLevel),
-    )
-    drawBlock(p5, () => {
-      p5.background(95)
+    heart.add(step)
 
-      p5.fill(p5.frameCount % 360, 100, 100) // 赤で塗りつぶし
-      p5.strokeWeight(10)
+    const { topPoint } = heart.get()
 
-      p5.beginShape()
-      p5.vertex(topPoint.x, topPoint.y) // 上の頂点
+    // 画面外に出たら、stepを反転
+    if (topPoint.y + 100 < 0 || topPoint.y + 100 > p5.height) {
+      step.y *= -1
+    }
+    if (topPoint.x < 0 || topPoint.x > p5.width) {
+      step.x *= -1
+    }
 
-      // ベジェ曲線でハートのカーブを表現
-      p5.bezierVertex(
-        leftControl1.x,
-        leftControl1.y, // 左側のコントロールポイント1
-        leftControl2.x,
-        leftControl2.y, // 左側のコントロールポイント2
-        underPoint.x,
-        underPoint.y, // 下の頂点
-      )
-      // 右側
-      p5.bezierVertex(
-        rightControl1.x,
-        rightControl1.y, // 右側のコントロールポイント2 (左右対称に調整)
-        rightControl2.x,
-        rightControl2.y, // 右側のコントロールポイント1 (左右対称に調整)
-        topPoint.x,
-        topPoint.y, // 上の頂点
-      )
-
-      p5.endShape(p5.CLOSE) // パスを閉じて塗りつぶし
-    })
+    p5.background(95)
+    heart.draw()
   }
 }
 
